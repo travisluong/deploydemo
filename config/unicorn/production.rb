3b896803395c3e0e44ae2a9f0ec5738d657929fb
Travis Luong <travis@travisluong.com>
2015-03-20 18:10:35 -0700

    unicorn

---
+++ config/unicorn/production.rb
@@ -0,0 +1,7 @@
+working_directory '/var/www/deploydemo/current'
+pid '/var/www/deploydemo/current/tmp/pids/unicorn.pid'
+stderr_path '/var/www/deploydemo/log/unicorn.log'
+stdout_path '/var/www/deploydemo/log/unicorn.log'
+listen '/tmp/unicorn.deploydemo.sock'
+worker_processes 2
+timeout 30
